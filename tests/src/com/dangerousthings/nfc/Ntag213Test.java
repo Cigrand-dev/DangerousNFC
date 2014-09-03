059faf10bda7c2d2ed3f09d9df81c34581cc37a3
Drew Carey Buglione <drew@dangerousthings.com>
2014-09-02 23:01:39 -0700

    NTAG215 and NTAG213 tests

---
+++ tests/src/com/dangerousthings/nfc/Ntag213Test.java
@@ -0,0 +1,66 @@
+package com.dangerousthings.nfc;
+
+import com.dangerousthings.nearfield.fake.FakeNtag213;
+import com.dangerousthings.nearfield.utils.DispatchUtils;
+
+import android.test.ActivityInstrumentationTestCase2;
+import android.test.MoreAsserts;
+
+import com.robotium.solo.Solo;
+import org.apache.commons.lang3.RandomStringUtils;
+
+import java.nio.charset.Charset;
+
+public class Ntag213Test extends ActivityInstrumentationTestCase2<MainActivity> {
+    Solo solo;
+    MainActivity activity;
+
+    public Ntag213Test() {
+        super("com.dangerousthings.nfc", MainActivity.class);
+    }
+
+    public void setUp() throws Exception {
+        activity = getActivity();
+        solo = new Solo(getInstrumentation(), activity);
+    }
+
+    public void testVirginTag() throws Exception {
+        String test_password_string = RandomStringUtils.randomAlphanumeric(4);
+        byte[] test_password_bytes = test_password_string.getBytes(Charset.forName("UTF-8"));
+        activity.setPasswordBytes(test_password_bytes);
+
+        FakeNtag213 tag = FakeNtag213.newInstance();
+
+        activity.onNewIntent(DispatchUtils.createDiscoveryIntent(tag));
+
+        solo.waitForDialogToOpen();
+        assertTrue(solo.searchText("Success! Tag has been updated! :)"));
+        solo.clickOnButton("Okay");
+
+        MoreAsserts.assertEquals(tag.getCC(), MainActivity.NEW_NTAG213_CC);
+        MoreAsserts.assertEquals(tag.getStaticLockBytes(), MainActivity.NEW_STATIC_LOCK_BYTES);
+        MoreAsserts.assertEquals(tag.getDynamicLockBytes(), MainActivity.NEW_NTAG213_DYNAMIC_LOCK_BYTES);
+        MoreAsserts.assertEquals(tag.getPass(), test_password_bytes);
+        MoreAsserts.assertEquals(tag.getPack(), MainActivity.NEW_PACK);
+        assertEquals(tag.getAuth0(), MainActivity.NEW_NTAG213_AUTH0);
+    }
+
+    public void testTagWithBadCC() throws Exception {
+        String test_password_string = RandomStringUtils.randomAlphanumeric(4);
+        byte[] test_password_bytes = test_password_string.getBytes(Charset.forName("UTF-8"));
+        activity.setPasswordBytes(test_password_bytes);
+
+        FakeNtag213 tag = FakeNtag213.newInstance();
+        tag.setCC(new byte[] { (byte)0xFF, (byte)0xFF, (byte)0xFF, (byte)0xFF });
+
+        activity.onNewIntent(DispatchUtils.createDiscoveryIntent(tag));
+
+        solo.waitForDialogToOpen();
+        assertTrue(solo.searchText("Error: Bad Capability Container"));
+        solo.clickOnButton("Okay");
+    }
+
+    public void tearDown() throws Exception {
+        solo.finishOpenedActivities();
+    }
+}
