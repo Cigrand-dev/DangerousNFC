8cd60eae087b7cf1227a0143cfb23fbe27bd8100
Drew Carey Buglione <drew@dangerousthings.com>
2014-09-02 22:21:00 -0700

    Improve test architecture (and coverage!)

--- tests/src/com/dangerousthings/nfc/MainActivityTest.java
+++ tests/src/com/dangerousthings/nfc/MainActivityTest.java
@@ -1,13 +1,5 @@
 package com.dangerousthings.nfc;
 
-import com.dangerousthings.nearfield.fake.FakeNtag216;
-import com.dangerousthings.nearfield.fake.FakeTagService;
-import com.dangerousthings.nearfield.fake.TagService;
-import com.dangerousthings.nearfield.reflect.TagReflection;
-import com.dangerousthings.nearfield.utils.DispatchUtils;
-
-import android.content.Intent;
-import android.os.Bundle;
 import android.test.ActivityInstrumentationTestCase2;
 import android.test.MoreAsserts;
 
@@ -17,9 +9,8 @@ import org.apache.commons.lang3.RandomStringUtils;
 import java.nio.charset.Charset;
 
 public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
-    private Solo solo;
+    Solo solo;
     MainActivity activity;
-    TagService tag_service = FakeTagService.getInstance();
 
     public MainActivityTest() {
         super("com.dangerousthings.nfc", MainActivity.class);
@@ -45,25 +36,6 @@ public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActiv
         MoreAsserts.assertEquals(test_password_bytes, activity.getPasswordBytes());
     }
 
-    public void testUnalteredTag() throws Exception {
-        String test_password_string = RandomStringUtils.randomAlphanumeric(4);
-        byte[] test_password_bytes = test_password_string.getBytes(Charset.forName("UTF-8"));
-        activity.setPasswordBytes(test_password_bytes);
-
-        FakeNtag216 tag = FakeNtag216.newInstance();
-        tag_service.add(tag);
-
-        Intent intent = DispatchUtils.createDiscoveryIntent(tag_service, tag);
-        activity.onNewIntent(intent);
-    }
-
-    // public void testTagWithBadCC() throws Exception {
-    // }
-
-    // public void testTagWithBadDynamicLockBytes() throws Exception {
-    // }
-
-    @Override
     public void tearDown() throws Exception {
         solo.finishOpenedActivities();
     }
